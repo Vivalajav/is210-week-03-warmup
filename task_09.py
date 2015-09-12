d71efdd5d4d5c80264933ca5534f7ca85a86e83a
Vivalajav <javkhan13@yahoo.com>
2015-09-12 22:19:20 +0000

    I've added a multiline doc string

--- task_09.py
+++ task_09.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+
+infinite improbability and the answer to everything.
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
