909598819cc365a5090dc603ba29ebb1590c3bdf
Vivalajav <javkhan13@yahoo.com>
2015-09-12 23:51:22 +0000

    Completed task 12, added all required values

---
+++ task_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Basic Decimal and Fraction Values"""
+
+import decimal
+import fractions
+
+INTVAL = 1
+
+FLOATVAL = decimal.Decimal('0.1')
+
+DECVAL = decimal.Decimal('0.1')
+
+FRACVAL = fractions.Fraction(1,10)
