404c397c9412f334e5b7c4ac7be636146b130b86
Vivalajav <javkhan13@yahoo.com>
2015-09-13 01:19:42 +0000

    Completed task 14 added all variables

---
+++ task_14.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""True & False values & Integer Equivalency"""
+
+IS_TRUE = True
+
+IS_FALSE = False
+
+IS_NONE = None
+
+INTEGER_EQUIV = True
+
+print IS_TRUE == 1, IS_FALSE == 0
