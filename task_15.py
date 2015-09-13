7e38c1aed3a9cb4eb212f984c826df6893e0ce8c
Vivalajav <javkhan13@yahoo.com>
2015-09-13 01:46:46 +0000

    Added the variable from the two combined

--- task_15.py
+++ task_15.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
 """Provides variables for string and integer conversion."""
 
-
 NOT_THE_QUESTION = 'The answer to life, the universe, and everything? It\'s '
-ANSWER = 42
+ANSWER = '42'
+
+print str(NOT_THE_QUESTION + ANSWER) 
+
+THANKS_FOR_THE_FISH = ('The answer to life, the universe, and everything?'
+                       'It\'s 42')
